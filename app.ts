c27024e644d0b1f7ea4dae3979cea7e763f285c6
Parth Doshi <parthdoshi480@gmail.com>
2024-06-26 11:34:15 +0530

    origin change

--- app.ts
+++ app.ts
@@ -23,7 +23,7 @@ app.use(cookieParser());
 // cors => cross origin resource sharing
 app.use(
   cors({
-    origin: ["http://localhost:3000","https://midas-nextjs-server.vercel.app"],
+    origin: ["*"],
     credentials: true,
   })
 );
